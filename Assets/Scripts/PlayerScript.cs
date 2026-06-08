b9906d1232dc1916922f3dd421fdd8ff5025ccf6
Gabriel Machado <Biel_machado08@hotmail.com>
2026-06-08 00:33:05 -0300

    Animation Player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-
+    private bool isWalking;
     [SerializeField]private float moveSpeed = 5f;
     private void Update(){
         Vector2 inputVector = new Vector2(0,0);
@@ -24,7 +24,15 @@ public class PlayerScript : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
 }
